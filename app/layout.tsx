44abb0f71c18864e1bd4762e2bedb02777e67712
zhaogongchegsi <zzh1586169624@163.com>
2026-03-10 14:37:24 +0800

    feat: clean up imports, remove baseUrl from tsconfig, and add CSS module declaration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import { Geist, Geist_Mono, Inter } from "next/font/google"
+import { Geist_Mono, Inter } from "next/font/google"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
